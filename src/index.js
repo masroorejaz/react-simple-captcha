c4ab7df023ac47d740a0a544b7c9e452096d7ee7
kumartul <kumartul001@gmail.com>
2022-04-16 13:54:11 +0530

    No need to use serviceWorker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import CaptchaTest from './captcha_test';
-import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
@@ -12,4 +11,3 @@ ReactDOM.render(
         <CaptchaTest />
     </BrowserRouter>, document.getElementById('root'));
 
-serviceWorker.unregister();

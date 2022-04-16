f69903db294336343bb20d90bf912e8e50579a66
kumartul <kumartul001@gmail.com>
2022-04-16 14:53:51 +0530

    [Refactor]: Remove unncessary blank lines and prefer 'const'

--- src/captcha_test.jsx
+++ src/captcha_test.jsx
@@ -1,24 +1,19 @@
 import React, { Component } from 'react'; 
-import { loadCaptchaEnginge, LoadCanvasTemplate, LoadCanvasTemplateNoReload, validateCaptcha } from 'react-simple-captcha';
- 
-
-
+import { loadCaptchaEnginge, LoadCanvasTemplate, LoadCanvasTemplateNoReload, validateCaptcha } from './hooks/react-simple-captcha';
 
 class CaptchaTest extends Component {
-
     componentDidMount () {
         loadCaptchaEnginge(6,'white','black'); 
     };
 
     doSubmit = () => {
-        let user_captcha = document.getElementById('user_captcha_input').value;
+        const user_captcha = document.getElementById('user_captcha_input').value;
 
         if (validateCaptcha(user_captcha)==true) {
             alert('Captcha Matched');
             loadCaptchaEnginge(6); 
             document.getElementById('user_captcha_input').value = "";
         }
-
         else {
             alert('Captcha Does Not Match');
             document.getElementById('user_captcha_input').value = "";
@@ -26,30 +21,25 @@ class CaptchaTest extends Component {
     };
  
     render() {
-         
+        return (
+            <div>
+                <div className="container">
+                    <div className="form-group">
+                        <div className="col mt-3">
+                            <LoadCanvasTemplate reloadText="Reload The Damn Captcha" reloadColor="red" />
+                        </div>
 
-        return (<div>
-            <div className="container">
-                <div className="form-group">
-                     
+                        <div className="col mt-3">
+                            <div><input placeholder="Enter Captcha Value" id="user_captcha_input" name="user_captcha_input" type="text"></input></div>
+                        </div>
 
-                    <div className="col mt-3">
-                        < LoadCanvasTemplate reloadText="Reload The Damn Captcha" reloadColor="red" />
+                        <div className="col mt-3">
+                            <div><button className="btn btn-primary" onClick={() => this.doSubmit()}>Submit</button></div>
+                        </div>
                     </div>
-
-                   
-                    <div className="col mt-3">
-                        <div><input placeholder="Enter Captcha Value" id="user_captcha_input" name="user_captcha_input" type="text"></input></div>
-                    </div>
-
-                    <div className="col mt-3">
-                        <div><button className="btn btn-primary" onClick={() => this.doSubmit()}>Submit</button></div>
-                    </div>
-                      
                 </div>
-
             </div>
-        </div>);
+        );
     };
 }
 
